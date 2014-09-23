aabea01500e89e03e7755a60a153f9b3165c06ce
Leander_K <Leander.Kurscheidt@gmx.de>
2014-09-23 22:00:57 +0200

    Added EventManager Class and testing directory
    I have added the EventManager Class and documented it, there is also a unit test available. The class passed all tests, so there should be no major bugs left.

--- src/karlskrone/jarvis/events/EventManager.java
+++ src/karlskrone/jarvis/events/EventManager.java
@@ -1,5 +1,143 @@
 package karlskrone.jarvis.events;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * This class is used to manage events.
+ * Activators can register with an id to fire events und ContentGenerators can subscribe to them.
+ *
+ * Event-IDs are used in the following form: package.class.nameOfTheEvent
+ */
 public class EventManager {
 
+    //here are all the ContentGenerators-Listeners stored
+    private final HashMap<String, ArrayList<ActivatorEventListener>> listeners = new HashMap<>();
+    //here are all the Instances to fire events stored
+    private final HashMap<String, ArrayList<ActivatorEventCaller>> callers = new HashMap<>();
+
+    /**
+     * Registers with the EventManager to fire an event.
+     *
+     * Multiple activators can register the same event and fire the same event.
+     *
+     * @param id the ID of the Event
+     * @return returns an ActivatorEventCaller, ActivatorEventCaller.fire() will fire an event
+     */
+    public ActivatorEventCaller registerActivatorEvent(String id) {
+        ActivatorEventCaller activatorEventCaller = new ActivatorEventCaller(id);
+        ArrayList<ActivatorEventCaller> callers = this.callers.get(id);
+        if (callers == null) {
+            this.callers.put(id, new ArrayList<>());
+            callers = this.callers.get(id);
+        }
+        callers.add(activatorEventCaller);
+        return activatorEventCaller;
+    }
+
+    /**
+     * Unregisters with the EventManager.
+     *
+     * @param id the ID of the Event
+     * @param call the ActivatorEventCaller-instance which was used by the class to fire the event
+     */
+    public void unregisterActivatorEvent(String id, ActivatorEventCaller call) {
+        call.setId("-1");
+        ArrayList<ActivatorEventCaller> callers = this.callers.get(id);
+        if (callers == null) {
+            return;
+        }
+        callers.remove(call);
+    }
+
+    /**
+     * Adds an listener for events from the activators.
+     *
+     * @param id the ID of the Event
+     * @param activatorEventListener the ActivatorEventListener-interface for receiving activator events
+     */
+    public void addActivatorEventListener (String id, ActivatorEventListener activatorEventListener) {
+        ArrayList<ActivatorEventListener> listenersList = listeners.get(id);
+        if (listenersList == null) {
+            listeners.put(id, new ArrayList<>());
+            listenersList = listeners.get(id);
+        }
+        listenersList.add(activatorEventListener);
+    }
+
+    /**
+     * Removes an listener for events from the activators
+     *
+     * @param id the ID of the Event
+     * @param activatorEventListener the ActivatorEventListener used to listen for events
+     */
+    public void deleteActivatorEventListener (String id, ActivatorEventListener activatorEventListener) {
+        ArrayList<ActivatorEventListener> listenersList = listeners.get(id);
+        if (listenersList == null) {
+            return;
+        }
+        listenersList.remove(activatorEventListener);
+    }
+
+    /**
+     * this method actually used to fire an event.
+     *
+     * @param id the ID of the Event
+     * @throws Exception an Exception will be thrown if there are currently other events fired
+     */
+    @SuppressWarnings("RedundantThrows")
+    private void fireActivatorEvent(String id) throws Exception{
+        ArrayList<ActivatorEventListener> contentGeneratorListeners = this.listeners.get(id);
+        //TODO: Protect EventManager from multiple events fired simultaneously
+        if (contentGeneratorListeners == null) {
+            return;
+        }
+        for (ActivatorEventListener next : contentGeneratorListeners) {
+            next.activatorEventFired(id);
+        }
+
+    }
+
+    /**
+     * The listener interface for receiving activator events.
+     *
+     * To receive events a class must implements this interface and register with the addActivatorEventListener-method.
+     * When the activator event occurs, that object's activatorEventFired method is invoked.
+     */
+    public interface ActivatorEventListener {
+
+        /**
+         * Invoked when an activator-event occurs.
+         * @param id the ID of the Event
+         */
+        public void activatorEventFired(String id);
+    }
+
+    /**
+     * The class for firing events.
+     *
+     * To fire events a class must register with registerActivatorEvent, then this class will be returned.
+     * Use fire() to fire the event;
+     */
+    public class ActivatorEventCaller {
+        private String id;
+        //private, so that this class can only constructed by EventManager
+        private ActivatorEventCaller(String id) {
+            this.id = id;
+        }
+
+        //private, so that this class can only called by EventManager
+        private void setId (String id) {
+            this.id = id;
+        }
+
+        /**
+         * This method is used to fire the event.
+         *
+         * @throws Exception an Exception will be thrown if there are currently other events fired
+         */
+        public void fire() throws Exception {
+            fireActivatorEvent(id);
+        }
+    }
 }
---
+++ test/karlskrone/jarvis/events/EventManagerTest.java
@@ -0,0 +1,59 @@
+package karlskrone.jarvis.events;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EventManagerTest {
+    static EventManager manager;
+
+    @Before
+    public void setUp() throws Exception {
+        manager = new EventManager();
+    }
+
+    @Test
+    public void testRegisterActivatorEvent() throws Exception {
+        EventManager.ActivatorEventCaller caller = manager.registerActivatorEvent("1");
+        final boolean[] isWorking = {false};
+        manager.addActivatorEventListener("1", id -> isWorking[0] = true);
+        caller.fire();
+        assertTrue(isWorking[0]);
+    }
+
+    @Test
+    public void testUnregisterActivatorEvent() throws Exception {
+        EventManager.ActivatorEventCaller caller = manager.registerActivatorEvent("2");
+        final boolean[] isWorking = {false};
+        manager.unregisterActivatorEvent("2", caller);
+        manager.addActivatorEventListener("2", id -> isWorking[0] = true);
+        caller.fire();
+        assertFalse(isWorking[0]);
+    }
+
+    @Test
+    public void testAddActivatorEventListener() throws Exception {
+        EventManager.ActivatorEventCaller caller = manager.registerActivatorEvent("3");
+        final boolean[] isWorking = {false};
+        manager.addActivatorEventListener("3", id -> isWorking[0] = true);
+        caller.fire();
+        assertTrue(isWorking[0]);
+    }
+
+    @Test
+    public void testRemoveActivatorEventListener() throws Exception {
+        EventManager.ActivatorEventCaller caller = manager.registerActivatorEvent("4");
+        final boolean[] isWorking = {false};
+        EventManager.ActivatorEventListener listener1;
+        manager.addActivatorEventListener("4", listener1 = new EventManager.ActivatorEventListener() {
+            @Override
+            public void activatorEventFired(String id) {
+                isWorking[0] = true;
+            }
+        });
+        manager.deleteActivatorEventListener("4", listener1);
+        caller.fire();
+        assertFalse(isWorking[0]);
+    }
+}
