ac7f1453871bbe5dabe0e8b3d027399e3115cdb3
Julian Brendl <julianbrendl@gmail.com>
2014-11-11 13:26:10 +0100

    added PropertiesContainer documentation

--- src/main/java/intellimate/izou/addon/PropertiesContainer.java
+++ src/main/java/intellimate/izou/addon/PropertiesContainer.java
@@ -8,14 +8,25 @@ import java.util.Properties;
 public class PropertiesContainer {
     Properties properties;
 
+    /**
+     * creates a new properties-container
+     */
     public PropertiesContainer() {
         this.properties = new Properties();
     }
 
+    /**
+     * gets the properties-container
+     * @return the properties-container
+     */
     public Properties getProperties() {
         return properties;
     }
 
+    /**
+     * sets the properties-container
+     * @param properties the properties-container
+     */
     public void setProperties(Properties properties) {
         this.properties = properties;
     }
