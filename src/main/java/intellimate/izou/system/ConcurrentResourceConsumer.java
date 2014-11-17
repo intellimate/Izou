f89f6e157f617c7b77e878af5cc9e6fd5128d0c5
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2014-11-17 12:33:34 +0100

    deprecated

--- src/main/java/intellimate/izou/system/ConcurrentResourceConsumer.java
+++ src/main/java/intellimate/izou/system/ConcurrentResourceConsumer.java
@@ -5,6 +5,7 @@ import java.util.List;
 /**
  * Use this interface to access Resources from other parts of the application
  */
+@Deprecated
 public interface ConcurrentResourceConsumer {
     /**
      * this method gets called, when the resources get delivered.
