e72d94e0c1cfcb42ef5b10e8b519589931a62d27
Julian Brendl <julianbrendl@gmail.com>
2014-12-01 21:27:14 +0100

    changed priorities

--- src/main/java/intellimate/izou/events/EventBehaviourController.java
+++ src/main/java/intellimate/izou/events/EventBehaviourController.java
@@ -10,8 +10,6 @@ import java.util.function.Function;
  * This class can control the Behaviour of the the Event, like which Output-Plugin should get the Event first.
  */
 public class EventBehaviourController {
-    public static final int MostPriority = 0;
-    public static final int LeastPriority = 1;
     private final Event event;
     private Function<List<Identification>, HashMap<Integer, List<Identification>>> outputPluginBehaviour;
     protected EventBehaviourController(Event event) {
@@ -22,8 +20,12 @@ public class EventBehaviourController {
      * this method sets the controls for the Output-Plugin Behaviour.
      * <p>
      * Supply a Function, which controls the OutputPlugin-Behaviour. You can set Priorities.
+     * The output-plugin with the highest POSITIVE priority (in int) will be processed first. Negative priorities
+     * are processed last (so outputPlugins with no priorities will be processed in between positive and negative
+     * priorities)
+     *
      * This function returns an HashMap, where the keys represent the associated Behaviour
-     * (use the Constants) and the values the Identification;
+     * and the values the Identification;
      * </p>
      * @param outputPluginBehaviour all the registered outputPlugins
      */
