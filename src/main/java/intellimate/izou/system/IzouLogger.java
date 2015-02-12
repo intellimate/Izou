fba01f49c91785393b66906eb3e84550db17a78c
Julian Brendl <julianbrendl@gmail.com>
2015-02-12 21:20:15 +0100

    addons now log debug level and up (went from warn to debug)

--- src/main/java/intellimate/izou/system/IzouLogger.java
+++ src/main/java/intellimate/izou/system/IzouLogger.java
@@ -50,14 +50,14 @@ public class IzouLogger {
 
             //adds appenders to an array called refs. It will later serve as references to the logger as to what
             // appenders it has
-            AppenderRef fileRef = AppenderRef.createAppenderRef("file", Level.WARN, null);
+            AppenderRef fileRef = AppenderRef.createAppenderRef("file", Level.DEBUG, null);
             AppenderRef consoleRef = AppenderRef.createAppenderRef("console", Level.DEBUG, null);
             AppenderRef[] refs = new AppenderRef[]{fileRef, consoleRef};
 
             //creates the logger configurations for the logger, where the appender-references are also added
             LoggerConfig loggerConfig = LoggerConfig.createLogger("false", Level.DEBUG, addOnId,
                     "true", refs, null, config, null);
-            loggerConfig.addAppender(fileAppender, Level.WARN, null);
+            loggerConfig.addAppender(fileAppender, Level.DEBUG, null);
             loggerConfig.addAppender(consoleAppender, Level.DEBUG, null);
 
             //finally creates the logger and returns it
