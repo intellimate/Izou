be44918a48fb788d8d26b7ab00473f9211ff82ac
Julian Brendl <julianbrendl@gmail.com>
2015-05-03 00:36:22 +0200

    did some basic layouting for the sound stuff

--- src/main/java/org/intellimate/izou/security/IzouPermissionManager.java
+++ src/main/java/org/intellimate/izou/security/IzouPermissionManager.java
@@ -1,10 +1,30 @@
 package org.intellimate.izou.security;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * The PermissionManager manages conflicts between addOns. For example if two AddOns want to play music, then the
  * PermissionManager will sort out this conflict. It manages intra-izou problems, that means it does not worry about
- * general system security. That is what the Izou
+ * general system security. That is what the {@link IzouSecurityManager} does.
  */
-public class IzouPermissionManager {
+public abstract class IzouPermissionManager {
 
+    /**
+     * Applies SHA-256 hash function on the {@code input} string
+     *
+     * @param input the string to apply to SHA-256 hash on
+     * @return the hashed input string
+     * @throws NoSuchAlgorithmException thrown if SHA-256 is not found as an algorithm
+     */
+    protected String sha256(String input) throws NoSuchAlgorithmException {
+        MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte aResult : result) {
+            sb.append(Integer.toString((aResult & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString().toLowerCase();
+    }
 }
--- src/main/java/org/intellimate/izou/security/IzouSoundPermissionManager.java
+++ src/main/java/org/intellimate/izou/security/IzouSoundPermissionManager.java
@@ -1,18 +1,95 @@
 package org.intellimate.izou.security;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by julianbrendl on 5/2/15.
+ * The IzouSoundPermissionManager manages conflicts between addOns regarding audio output. For example if two AddOns
+ * want to play music, then the IzouSoundPermissionManager will decide who gets to play it.
  */
-public class IzouSoundPermissionManager {
+public class IzouSoundPermissionManager extends IzouPermissionManager {
     private final List<String> registeredAddOns;
+    private String currentPlaybackID;
+    private boolean isPlaying;
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
 
+    /**
+     * Creates a new instance of IzouSoundPermissionManager
+     */
     public IzouSoundPermissionManager() {
-        this.registeredAddOns = new ArrayList<>();
+        registeredAddOns = new ArrayList<>();
+        currentPlaybackID = null;
+        isPlaying = false;
     }
 
+    /**
+     * Adds an addOn to the registered addOns list. Only addOns on that list can request to play sound
+     *
+     * @param addOnID the ID of the addOn to add
+     */
+    public void registerAddOn(String addOnID) {
+        registeredAddOns.add(addOnID);
+    }
 
+    /**
+     * Checks if an addOn is registered to request audio playback
+     *
+     * @param addOnID the id of the addOn to check
+     * @return true if the addOn is registered to request audio playback, else false
+     */
+    public boolean isRegistered(String addOnID) {
+        return registeredAddOns.contains(addOnID);
+    }
+
+    /**
+     * Requests permission to play sound, if the permission is granted, the {@link IzouSecurityManager} will let the
+     * addOn with {@code addOnID} play sound, else it will block it.
+     * <p>
+     * There is always ONLY ONE permission that is given out to an addOn, (first come, first serve) and if the
+     * permission has already been given out, then the requesting addOn will have to wait until it is returned. Once
+     * the this method returns true, the requesting addOn will have been registered as the currently playing addOn, so
+     * it is responsible for calling the {@link #returnPlaybackPermission(String)} method to signal it is done. Only
+     * then can the next addOn play sound again.
+     * </p>
+     * @param addOnID the addOn id of the addOn that is requesting permission to play sound
+     * @return true if the permission was granted, false if it was denied
+     */
+    public boolean requestPlaybackPermission(String addOnID) {
+        if (!isPlaying) {
+            try {
+                currentPlaybackID = sha256(addOnID);
+            } catch (NoSuchAlgorithmException e) {
+                logger.error("Did not find sha256 algorithm", e);
+            }
+            isPlaying = true;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * If this method is called from the addOn currently playing sound, then the lock on that addOn is given up and
+     * anyone can request to play sound again. If the permission was successfully returned, true is returned and
+     * otherwise false. (For instance if {@code addOnID} does not match the currently playing addOn ID)
+     *
+     * @param addOnID the addOnID of the addOn requesting to return its audio permission
+     * @return true if the permission was returned successfully, else false
+     */
+    public boolean returnPlaybackPermission(String addOnID) {
+        try {
+            if (sha256(addOnID).equals(currentPlaybackID)) {
+                isPlaying = false;
+                currentPlaybackID = null;
+                return true;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            logger.error("Did not find sha256 algorithm", e);
+        }
+        return false;
+    }
 }
