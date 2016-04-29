1c54fb10fd88282f606fbc882c101c36cf895515
LeanderK <Leander.Kurscheidt@gmx.de>
2016-04-29 21:52:10 +0200

    config: first config commit

---
+++ src/main/java/org/intellimate/izou/config/Config.java
@@ -0,0 +1,19 @@
+package org.intellimate.izou.config;
+
+import java.util.List;
+
+/**
+ * @author LeanderK
+ * @version 1.0
+ */
+public class Config {
+    /**
+     * the token used to communicate with the server
+     */
+    public String token;
+
+    /**
+     * the configured apps
+     */
+    public List<AddOn> addOns;
+}
