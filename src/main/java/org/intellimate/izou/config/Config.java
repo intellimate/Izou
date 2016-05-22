76d6e6b240358b4b76a04f0e397cc3647a8c693f
LeanderK <Leander.Kurscheidt@gmx.de>
2016-05-22 20:28:17 +0200

    config: added IzouUrl, IzouSocketUrl, and ssl configuration option

--- src/main/java/org/intellimate/izou/config/Config.java
+++ src/main/java/org/intellimate/izou/config/Config.java
@@ -11,4 +11,19 @@ public class Config {
      * the token used to communicate with the server
      */
     public String token;
+
+    /**
+     * the url of the server
+     */
+    public String url = "api.izou.info";
+
+    /**
+     * the url of the server for the socket connection
+     */
+    public String urlSocket = "api.izou.info";
+
+    /**
+     * true if ssl-connection is enabled, false if not
+     */
+    public String ssl = "true";
 }
